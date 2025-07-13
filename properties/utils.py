1d3c713dba3ade139ca2a7484bbf33807d442c4c
Michael Mwangi <michaelmichire2016@gmail.com>
2025-07-13 11:50:37 +0300

    Cache the Property queryset in Redis for 1 hour using Django’s low-level cache API

---
+++ properties/utils.py
@@ -0,0 +1,12 @@
+from django.core.cache import cache
+from .models import Property
+
+def get_all_properties():
+    properties = cache.get('all_properties')
+    if properties is None:
+        properties = list(Property.objects.all().values(
+            'id', 'title', 'description', 'price', 'location', 'created_at'
+        ))
+        cache.set('all_properties', properties, timeout=3600)  # cache for 1 hour
+    return properties
+
--- properties/views.py
+++ properties/views.py
@@ -7,7 +7,5 @@ from .models import Property
 
 @cache_page(60 * 15)  # Cache for 15 minutes
 def property_list(request):
-    properties = Property.objects.all().values(
-        'id', 'title', 'description', 'price', 'location', 'created_at'
-    )
-    return JsonResponse({'data': list(properties)})
+    properties = get_all_properties()
+    return JsonResponse({'data': properties})
